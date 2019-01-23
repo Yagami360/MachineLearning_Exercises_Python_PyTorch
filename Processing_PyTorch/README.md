5e0258fb64819179a1a8113fd82c30c4fa069629
Yagami360 <y034112@gmail.com>
2019-01-23 22:27:55 +0900

    first commit

---
+++ Processing_PyTorch/README.md
@@ -0,0 +1,19 @@
+# Processing_PyTorch
+PyTorch での処理フローの練習コード集。（※機械学習のコードではありません。）<br>
+
+## ■ 項目 [Contents]
+1. [動作環境](#動作環境)
+1. [使用法](#使用法)
+
+## ■ 動作環境
+
+- Python : 3.6
+- Anaconda : 5.0.1
+- PyTorch : 1.0.0
+
+## ■ 使用法
+
+- 使用法
+```
+$ python main.py
+```
---
+++ Processing_PyTorch/main1.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+# Anaconda 5.0.1 環境
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+
+
+def main():
+    """
+	OpenAI Gym での処理フローの練習コード
+    """
+    print("Start main()")
+
+    # バージョン確認
+    print( "PyTorch :", torch.__version__ )
+
+    print("Finish main()")
+    return
+
+    
+if __name__ == '__main__':
+     main()
