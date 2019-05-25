cc3344f4ad1d44815f249ba4e0f906cf54e9b7d2
Yagami360 <y034112@gmail.com>
2019-05-25 11:46:15 +0900

    ・ResNet  の実装追加

---
+++ ResNet_PyTorch/README.md
@@ -0,0 +1,199 @@
+# ResNet_PyTorch
+ResNet-18 の PyTorch での実装<br>
+
+- 参考コード
+    - [PyTorch ResNet: Building, Training and Scaling Residual Networks on PyTorch](https://missinglink.ai/guides/deep-learning-frameworks/pytorch-resnet-building-training-scaling-residual-networks-pytorch/)
+    - [ResNet PyTorch](http://www.pabloruizruiz10.com/resources/CNNs/ResNet-PyTorch.html)
+
+
+## ■ 項目 [Contents]
+1. [動作環境](#動作環境)
+1. [使用法](#使用法)
+1. [コードの実行結果](#コードの実行結果)
+1. [背景理論](http://yagami12.hatenablog.com/entry/2017/09/17/111935#ResNet%EF%BC%88%E6%AE%8B%E5%B7%AE%E3%83%8D%E3%83%83%E3%83%88%E3%83%AF%E3%83%BC%E3%82%AF%EF%BC%89)
+
+## ■ 動作環境
+
+- Windows 10
+- Geforce GTX1050 / VRAM:2GB
+- Python : 3.6
+- Anaconda : 5.0.1
+- PyTorch : 1.1.0
+
+## ■ 使用法
+
+- 使用法
+
+```
+$ python main.py
+```
+
+- CIFAR-10 の場合
+
+```
+$ python main.py --device GPU --dataset CIFAR-10 --n_epoches 10  --batch_size 32 --learning_rate 0.001
+```
+
+
+- 設定可能なコマンドライン引数
+
+|引数名|意味|値 (Default値)|
+|---|---|---|
+|`--device`|実行デバイス|`GPU` (Default) or `CPU`|
+|`--dataset`|データセット|`MNIST` (Default) or `CIFAR-10`|
+|`--dataset_path`|データセットの保存先|`./dataset` (Default)|
+|`--image_size`|データセットの画像サイズ（pixel単位）|`224` (Default)<br>ImageNetのサイズ|
+|`--n_classes`|分類クラス数|`10` (Default)|
+|`--n_epoches`|エポック数|`50` (Default)|
+|`--batch_size`|バッチサイズ|`32` (Default)|
+|`--learning_rate`|学習率|`0.001` (Default)|
+|`--result_path`|学習結果のフォルダ|`./result` (Default)|
+|`--xxx`|xxx|`xxx` (Default)|
+
+
+<a id="コードの実行結果"></a>
+
+## ■ コードの実行結果：`main.py`
+
+|パラメータ名|値（実行条件１）|値（実行条件２）|
+|---|---|---|
+|使用デバイス：`--device`|GPU|←|
+|データセット：`--dataset`|CIFAR-10|
+|画像サイズ：`--image_size`|224|
+|分類クラス数：`--n_classes`|10|
+|エポック数：`--n_epoches`|5||
+|バッチサイズ：`--batch_size`|32|←|
+|最適化アルゴリズム|Adam|←|
+|学習率：`--learning_rate`|0.001|←|
+|減衰率 beta1|0.5|←|
+|シード値|`random.seed(8)`<br>`np.random.seed(8)`<br>`torch.manual_seed(8)`|←|
+
+- ネットワーク構成（実行条件１）
+```python
+_model : ResNet18(
+  (_layer0): Sequential(
+    (0): Conv2d(3, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3))
+    (1): BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+    (2): LeakyReLU(negative_slope=0.2, inplace)
+    (3): MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False)
+  )
+  (_layer1): Sequential(
+    (0): BasicBlock(
+      (_layer1): Sequential(
+        (0): Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        (1): BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        (2): LeakyReLU(negative_slope=0.2, inplace)
+      )
+      (_layer2): Sequential(
+        (0): Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        (1): BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+      )
+      (_shortcut_connections): Sequential()
+    )
+    (1): BasicBlock(
+      (_layer1): Sequential(
+        (0): Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        (1): BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        (2): LeakyReLU(negative_slope=0.2, inplace)
+      )
+      (_layer2): Sequential(
+        (0): Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        (1): BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+      )
+      (_shortcut_connections): Sequential()
+    )
+  )
+  (_layer2): Sequential(
+    (0): BasicBlock(
+      (_layer1): Sequential(
+        (0): Conv2d(64, 128, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
+        (1): BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        (2): LeakyReLU(negative_slope=0.2, inplace)
+      )
+      (_layer2): Sequential(
+        (0): Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        (1): BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+      )
+      (_shortcut_connections): Sequential(
+        (0): Conv2d(64, 128, kernel_size=(1, 1), stride=(2, 2), bias=False)
+        (1): BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+      )
+    )
+    (1): BasicBlock(
+      (_layer1): Sequential(
+        (0): Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        (1): BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        (2): LeakyReLU(negative_slope=0.2, inplace)
+      )
+      (_layer2): Sequential(
+        (0): Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        (1): BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+      )
+      (_shortcut_connections): Sequential()
+    )
+  )
+  (_layer3): Sequential(
+    (0): BasicBlock(
+      (_layer1): Sequential(
+        (0): Conv2d(128, 256, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
+        (1): BatchNorm2d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        (2): LeakyReLU(negative_slope=0.2, inplace)
+      )
+      (_layer2): Sequential(
+        (0): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        (1): BatchNorm2d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+      )
+      (_shortcut_connections): Sequential(
+        (0): Conv2d(128, 256, kernel_size=(1, 1), stride=(2, 2), bias=False)
+        (1): BatchNorm2d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+      )
+    )
+    (1): BasicBlock(
+      (_layer1): Sequential(
+        (0): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        (1): BatchNorm2d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        (2): LeakyReLU(negative_slope=0.2, inplace)
+      )
+      (_layer2): Sequential(
+        (0): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        (1): BatchNorm2d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+      )
+      (_shortcut_connections): Sequential()
+    )
+  )
+  (_layer4): Sequential(
+    (0): BasicBlock(
+      (_layer1): Sequential(
+        (0): Conv2d(256, 512, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
+        (1): BatchNorm2d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        (2): LeakyReLU(negative_slope=0.2, inplace)
+      )
+      (_layer2): Sequential(
+        (0): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        (1): BatchNorm2d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+      )
+      (_shortcut_connections): Sequential(
+        (0): Conv2d(256, 512, kernel_size=(1, 1), stride=(2, 2), bias=False)
+        (1): BatchNorm2d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+      )
+    )
+    (1): BasicBlock(
+      (_layer1): Sequential(
+        (0): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        (1): BatchNorm2d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        (2): LeakyReLU(negative_slope=0.2, inplace)
+      )
+      (_layer2): Sequential(
+        (0): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        (1): BatchNorm2d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+      )
+      (_shortcut_connections): Sequential()
+    )
+  )
+  (_avgpool): AvgPool2d(kernel_size=7, stride=1, padding=0)
+  (_fc_layer): Linear(in_features=512, out_features=10, bias=True)
+)
+```
+
+### ◎ 損失関数のグラフ（実行条件１）
+
---
+++ ResNet_PyTorch/ResNet.py
@@ -0,0 +1,317 @@
+# -*- coding:utf-8 -*-
+import os
+import numpy as np
+from tqdm import tqdm
+
+# PyTorch
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision.utils import save_image
+
+
+class BasicBlock( nn.Module ):
+    """
+    """
+    def __init__( 
+        self,
+        device,
+        n_in_channels = 3,
+        n_out_channels = 3,
+        stride = 1,
+    ):
+        """
+        [Args]
+            n_in_channels : <int> 入力画像のチャンネル数
+            n_out_channels : <int> 出力画像のチャンネル数
+            stride : <int>
+        """
+        super( BasicBlock, self ).__init__()
+        self._device = device
+
+        self._layer1 = nn.Sequential(
+            nn.Conv2d( n_in_channels, n_out_channels, kernel_size=3, stride=stride, padding=1 ),
+            nn.BatchNorm2d( n_out_channels ),
+            nn.LeakyReLU( 0.2, inplace=True ),
+        ).to( self._device )
+
+        self._layer2 = nn.Sequential(
+            nn.Conv2d( n_out_channels, n_out_channels, kernel_size=3, stride=1, padding=1 ),
+            nn.BatchNorm2d( n_out_channels ),
+        ).to( self._device )
+
+        # shortcut connection は、恒等写像
+        self._shortcut_connections = nn.Sequential()
+
+        # 入出力次元が異なる場合は、ゼロパディングで、次元の不一致箇所を０で埋める。
+        if( n_in_channels != n_out_channels ):
+            self._shortcut_connections = nn.Sequential(
+                nn.Conv2d( n_in_channels, n_out_channels, kernel_size=1, stride=stride, padding=0,bias=False),
+                nn.BatchNorm2d( n_out_channels )
+            ).to( self._device )
+
+        return
+
+    def forward( self, x ):
+        out = self._layer1(x)
+        out = self._layer2(out)
+
+        # shortcut connection からの経路を加算
+        out += self._shortcut_connections(x)
+        return out
+
+
+class ResNet18( nn.Module ):
+    def __init__( 
+        self,
+        device,
+        n_in_channels = 3,
+        n_classes = 10
+    ):
+        super( ResNet18, self ).__init__()
+        
+        self._device = device
+        self._layer0 = nn.Sequential(
+            nn.Conv2d( n_in_channels, 64, kernel_size=7, stride=2, padding=3 ),
+            nn.BatchNorm2d( 64 ),
+            nn.LeakyReLU( 0.2, inplace=True ),
+            nn.MaxPool2d( kernel_size=3, stride=2, padding=1 )
+        ).to( self._device )
+        
+        self._layer1 = nn.Sequential(
+                BasicBlock(
+                    device = self._device,
+                    n_in_channels = 64, n_out_channels = 64, stride = 1
+                ),
+                BasicBlock(
+                    device = self._device,
+                    n_in_channels = 64, n_out_channels = 64, stride = 1
+                ),          
+        )
+
+        self._layer2 = nn.Sequential(
+                BasicBlock(
+                    device = self._device,
+                    n_in_channels = 64, n_out_channels = 128, stride = 2
+                ),
+                BasicBlock(
+                    device = self._device,
+                    n_in_channels = 128, n_out_channels = 128, stride = 1
+                ),          
+        )
+
+        self._layer3 = nn.Sequential(
+                BasicBlock(
+                    device = self._device,
+                    n_in_channels = 128, n_out_channels = 256, stride = 2
+                ),
+                BasicBlock(
+                    device = self._device,
+                    n_in_channels = 256, n_out_channels = 256, stride = 1
+                ),          
+        )
+
+        self._layer4 = nn.Sequential(
+                BasicBlock(
+                    device = self._device,
+                    n_in_channels = 256, n_out_channels = 512, stride = 2
+                ),
+                BasicBlock(
+                    device = self._device,
+                    n_in_channels = 512, n_out_channels = 512, stride = 1
+                ),          
+        )
+
+        self._avgpool = nn.AvgPool2d( 7, stride=1 ).to( self._device )
+        self._fc_layer = nn.Linear( 512, n_classes ).to( self._device )
+        return
+
+    def forward( self, x ):
+        out = self._layer0(x)
+        out = self._layer1(out)
+        out = self._layer2(out)
+        out = self._layer3(out)
+        out = self._layer4(out)
+        #out = torch.squeeze(out)
+        out = self._avgpool(out)
+        out = out.view( out.size(0), -1 )
+        out = self._fc_layer(out)
+        return out
+
+
+class ResNetClassifier( object ):
+    """
+    [public]
+    [protected] 変数名の前にアンダースコア _ を付ける
+        _device : <toech.cuda.device> 実行デバイス
+        _n_classes : 
+        _n_epoches : <int> エポック数（学習回数）
+        _batch_size : <int> ミニバッチ学習時のバッチサイズ
+        _learnig_rate : <float> 最適化アルゴリズムの学習率
+
+        _model : <nn.Module> ResNet のネットワークモデル
+        _loss_fn : 損失関数
+        _optimizer : <torch.optim.Optimizer> モデルの最適化アルゴリズム
+        _loss_historys : <list> 損失関数値の履歴（イテレーション毎）
+
+    [private] 変数名の前にダブルアンダースコア __ を付ける（Pythonルール）
+    """
+    def __init__(
+        self,
+        device,
+        n_classes = 10,
+        n_epoches = 10,
+        batch_size = 64,
+        learing_rate = 0.0001,
+    ):
+        self._device = device
+        self._n_classes = n_classes
+        self._n_epoches = n_epoches
+        self._batch_size = batch_size
+        self._learning_rate = learing_rate
+
+        self._loss_fn = None
+        self._optimizer = None
+        self._loss_historys = []
+
+        self.model()
+        self.loss()
+        self.optimizer()
+        return
+
+    def print( self, str = "" ):
+        print( "----------------------------------" )
+        print( "ResNetClassifier" )
+        print( self )
+        print( str )
+        print( "_device :", self._device )
+        print( "_n_classes :", self._n_classes )        
+        print( "_n_epoches :", self._n_epoches )
+        print( "_batch_size :", self._batch_size )
+        print( "_learning_rate :", self._learning_rate )
+        print( "_model :", self._model )
+        print( "_loss_fn :", self._loss_fn )
+        print( "_optimizer :", self._optimizer )
+        print( "----------------------------------" )
+        return
+
+    @property
+    def loss_history( self ):
+        return self._loss_historys
+
+    def model( self ):
+        """
+        モデルの定義を行う。
+        [Args]
+        [Returns]
+        """
+        self._model = ResNet18(
+            device = self._device,
+            n_in_channels = 3,
+            n_classes = self._n_classes
+        )
+        return
+
+    def loss( self ):
+        """
+        損失関数の設定を行う。
+        [Args]
+        [Returns]
+        """
+        self._loss_fn = nn.CrossEntropyLoss()
+        return
+
+    def optimizer( self ):
+        """
+        モデルの最適化アルゴリズムの設定を行う。
+        [Args]
+        [Returns]
+        """
+        self._optimizer = optim.Adam(
+            params = self._model.parameters(),
+            lr = self._learning_rate,
+            betas = (0.5,0.999)
+        )
+        return
+
+    def fit( self, dloader, n_sava_step = 5, result_path = "./result" ):
+        """
+        指定されたトレーニングデータで、モデルの fitting 処理を行う。
+        [Args]
+            dloader : <DataLoader> 学習用データセットの DataLoader
+            n_sava_step : <int> 学習途中での生成画像の保存間隔（イテレーション単位）
+            result_path : <str> 学習途中＆結果を保存するディレクトリ
+        [Returns]
+        """
+        if( os.path.exists( result_path ) == False ):
+            os.mkdir( result_path )
+
+        #-------------------------------------
+        # モデルを学習モードに切り替える。
+        #-------------------------------------
+        self._model.train()
+
+        #-------------------------------------
+        # 学習処理ループ
+        #-------------------------------------
+        iterations = 0      # 学習処理のイテレーション回数
+
+        print("Starting Training Loop...")
+        # エポック数分トレーニング
+        for epoch in tqdm( range(self._n_epoches), desc = "Epoches" ):
+            # DataLoader から 1minibatch 分取り出し、ミニバッチ処理
+            for (images,targets) in tqdm( dloader, desc = "minibatch process in DataLoader" ):
+                #print( "images.size() : ", images.size() )
+                #print( "targets.size() : ", targets.size() )
+
+                # 一番最後のミニバッチループで、バッチサイズに満たない場合は無視する
+                # （後の計算で、shape の不一致をおこすため）
+                if images.size()[0] != self._batch_size:
+                    break
+
+                iterations += 1
+
+                # ミニバッチデータを GPU へ転送
+                images = images.to( self._device )
+                targets = targets.to( self._device )
+
+                #----------------------------------------------------
+                # 勾配を 0 に初期化
+                # （この初期化処理が必要なのは、勾配がイテレーション毎に加算される仕様のため）
+                #----------------------------------------------------
+                self._optimizer.zero_grad()
+
+                #----------------------------------------------------
+                # 学習用データをモデルに流し込む
+                # model(引数) で呼び出せるのは、__call__ をオーバライトしているため
+                #----------------------------------------------------
+                output = self._model( images )
+
+                #----------------------------------------------------
+                # 損失関数を計算する
+                #----------------------------------------------------
+                loss = self._loss_fn( output, targets )
+                self._loss_historys.append( loss )
+
+                #----------------------------------------------------
+                # 誤差逆伝搬
+                #----------------------------------------------------
+                loss.backward()
+
+                #----------------------------------------------------
+                # backward() で計算した勾配を元に、設定した optimizer に従って、重みを更新
+                #----------------------------------------------------
+                self._optimizer.step()
+
+                #
+                print( "\nepoch = %d / iterations = %d / loss = %f" % ( epoch, iterations, loss ) )
+
+        print("Finished Training Loop.")
+        return
+
+
+    def save_model( self, dir = ".checkpoint/" ):
+        return
+
+    def load_model( self ):
+        return
